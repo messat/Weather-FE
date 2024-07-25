aa9a37925af8eacd7637bc97ed1e7378e2d64244
messat <148489126+messat@users.noreply.github.com>
2024-07-25 17:13:03 +0100

    Added new forecast to display UI

--- Weather-API/src/App.jsx
+++ Weather-API/src/App.jsx
@@ -2,6 +2,7 @@ import TopButtons from "./components/TopButtons"
 import Inputs from "./components/Inputs"
 import TimeAndLocation from "./components/TimeAndLocation"
 import TempAndDetails from "./components/TempAndDetails"
+import Forecast from "./components/Forecast"
  
 function App() {
    return (
@@ -11,6 +12,8 @@ function App() {
 
       <TimeAndLocation />
       <TempAndDetails />
+      <Forecast />
+      <Forecast />
      </div>
    )
  }
---
+++ Weather-API/src/components/Forecast.jsx
@@ -0,0 +1,30 @@
+
+function Forecast () {
+    const data = [1,2,3,4,5]
+  return (
+    <div>
+        <div className="flex items-center justify-start mt-6">
+           <p className="font-medium uppercase">3 hour step forecast</p>
+        </div>
+        <hr className="my-1"/>
+
+        <div className="flex items-center justify-between">
+            {data.map((num, index)=>(
+                <div key={index} className="flex flex-col items-center justify-center">
+                    <p className="font-light text-sm">Wed</p>
+                    <img 
+                    src="https://openweathermap.org/img/wn/10d@2x.png"
+                    alt="Weather Icon"
+                    className="w-12 my-1"
+                    />
+                    <p className="font-medium">12°</p>
+                
+                </div>
+            ))}
+
+        </div>
+    </div>
+  )
+}
+
+export default Forecast
